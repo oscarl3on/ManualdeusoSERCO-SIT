17cb6c9a8f4f817ca0f76194706ddff6c6d89e09
Oscar León <oscarleon.alcatel@gmail.com>
2022-06-20 16:39:46 -0600

    se soluciona el problema en el script para mostrar y ocultar respuestas

--- js/faqs.js
+++ js/faqs.js
@@ -1,10 +1,21 @@
 
-function mostrar() {
-    div = document.getElementById('flotante');
-    div.style.display = '';
+function mostrar_q1() {
+ div = document.getElementById('q1'); 
+ div.style.display = '';  
 }
 
-function cerrar() {
-    div = document.getElementById('flotante');
+function cerrar_q1() {
+    div = document.getElementById('q1');
     div.style.display = 'none';
 }
+
+
+function mostrar_q2() {
+    div = document.getElementById('q2'); 
+    div.style.display = '';  
+   }
+   
+   function cerrar_q2() {
+       div = document.getElementById('q2');
+       div.style.display = 'none';
+   }
