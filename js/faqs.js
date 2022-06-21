0e06c70dd035e5df2e91273f0eea257133ac5d90
Oscar León <oscarleon.alcatel@gmail.com>
2022-06-21 10:45:22 -0600

    se agrega el efecto de mostrar y ocultar en todas las preguntas

--- js/faqs.js
+++ js/faqs.js
@@ -1,7 +1,7 @@
 
 function mostrar_q1() {
- div = document.getElementById('q1'); 
- div.style.display = '';  
+    div = document.getElementById('q1'); 
+    div.style.display = '';  
 }
 
 function cerrar_q1() {
@@ -13,9 +13,73 @@ function cerrar_q1() {
 function mostrar_q2() {
     div = document.getElementById('q2'); 
     div.style.display = '';  
-   }
+}
    
-   function cerrar_q2() {
-       div = document.getElementById('q2');
-       div.style.display = 'none';
-   }
+function cerrar_q2() {
+    div = document.getElementById('q2');
+    div.style.display = 'none';
+}
+
+function mostrar_q3() {
+    div = document.getElementById('q3'); 
+    div.style.display = '';  
+}
+   
+function cerrar_q3() {
+    div = document.getElementById('q3');
+    div.style.display = 'none';
+}
+
+function mostrar_q4() {
+    div = document.getElementById('q4'); 
+    div.style.display = '';  
+}
+   
+function cerrar_q4() {
+    div = document.getElementById('q4');
+    div.style.display = 'none';
+}
+
+function mostrar_q5() {
+    div = document.getElementById('q5'); 
+    div.style.display = '';  
+}
+   
+function cerrar_q5() {
+    div = document.getElementById('q5');
+    div.style.display = 'none';
+}
+
+function mostrar_q6() {
+    div = document.getElementById('q6'); 
+    div.style.display = '';  
+}
+   
+function cerrar_q6() {
+    div = document.getElementById('q6');
+    div.style.display = 'none';
+}
+
+function mostrar_q7() {
+    div = document.getElementById('q7'); 
+    div.style.display = '';  
+}
+   
+function cerrar_q7() {
+    div = document.getElementById('q7');
+    div.style.display = 'none';
+}
+
+function mostrar_q8() {
+    div = document.getElementById('q8'); 
+    div.style.display = '';  
+}
+   
+function cerrar_q8() {
+    div = document.getElementById('q8');
+    div.style.display = 'none';
+}
+
+
+
+   
